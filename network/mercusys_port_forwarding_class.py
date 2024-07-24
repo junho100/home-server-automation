e55214781ff96b564c734e22c27e09883b4c4ff9
junho100 <bemodesty306@naver.com>
2024-07-24 13:26:19 +0900

    delete unused allocated vars

--- network/mercusys_port_forwarding_class.py
+++ network/mercusys_port_forwarding_class.py
@@ -11,58 +11,42 @@ class MercusysPortForwarding(BasePortForwarding):
     def create_port_forwarding(self, password, private_ip, port, server_port):
         driver = self._initialize_driver()
 
-        password_input = driver.find_element(By.XPATH, "//*[@type='password']")
+        driver.find_element(By.XPATH, "//*[@type='password']").send_keys(password)
 
-        password_input.send_keys(password)
+        driver.find_element(By.XPATH, "//*[@title='LOG IN']").click()
 
-        btn = driver.find_element(By.XPATH, "//*[@title='LOG IN']")
+        advanced_nav_parent_element = driver.find_element(By.CSS_SELECTOR, '[navi-value="advanced"]')
+        advanced_nav_parent_element.find_element(By.TAG_NAME, "a").click()
 
-        btn.click()
+        nat_nav_parent_element = driver.find_element(By.CSS_SELECTOR, '[navi-value="nat"]')
+        nat_nav_parent_element.click()
 
-        parent_element = driver.find_element(By.CSS_SELECTOR, '[navi-value="advanced"]')
-        advanced_nav = parent_element.find_element(By.TAG_NAME, "a")
-        advanced_nav.click()
-
-        parent_element2 = driver.find_element(By.CSS_SELECTOR, '[navi-value="nat"]')
-        parent_element2.click()
-
-        port_forwarding_nav = parent_element2.find_element(By.CSS_SELECTOR, '[navi-value="portForwarding"]')
-        port_forwarding_nav.click()
+        nat_nav_parent_element.find_element(By.CSS_SELECTOR, '[navi-value="portForwarding"]').click()
 
         add_btn = driver.find_element(By.CSS_SELECTOR, 'div#port-forwarding-grid_bar.operation-container')
         add_btn.click()
 
-        server_name = driver.find_element(By.XPATH,
-                                          '//*[@label-field="{PORT_FORWARDING.SERVICE_NAME}"]/div[2]/div[1]/span[2]/input')
-        server_name.send_keys("backend")
+        driver.find_element(By.XPATH,
+                                          '//*[@label-field="{PORT_FORWARDING.SERVICE_NAME}"]/div[2]/div[1]/span[2]/input').send_keys("backend")
 
-        ip = driver.find_element(By.XPATH,
-                                 '//*[@label-field="{PORT_FORWARDING.DEVICE_IP_ADDRESS}"]/div[2]/div[1]/span[2]/input')
-        ip.send_keys(private_ip)
+        driver.find_element(By.XPATH,
+                                 '//*[@label-field="{PORT_FORWARDING.DEVICE_IP_ADDRESS}"]/div[2]/div[1]/span[2]/input').send_keys(private_ip)
 
-        ex_port = driver.find_element(By.XPATH, '//*[@id="port-forwarding-external-port"]/div[2]/div[1]/span[2]/input')
-        in_port = driver.find_element(By.XPATH, '//*[@id="port-forwarding-internal-port"]/div[2]/div[1]/span[2]/input')
-
-        ex_port.send_keys(port)
-        in_port.send_keys(port)
+        driver.find_element(By.XPATH, '//*[@id="port-forwarding-external-port"]/div[2]/div[1]/span[2]/input').send_keys(port)
+        driver.find_element(By.XPATH, '//*[@id="port-forwarding-internal-port"]/div[2]/div[1]/span[2]/input').send_keys(port)
 
         driver.find_element(By.XPATH, '//*[@id="port-forwarding-grid-save-button"]/div[2]/div[1]/a').click()
 
         add_btn.click()
 
-        server_name = driver.find_element(By.XPATH,
-                                          '//*[@label-field="{PORT_FORWARDING.SERVICE_NAME}"]/div[2]/div[1]/span[2]/input')
-        server_name.send_keys("crypto")
+        driver.find_element(By.XPATH,
+                                          '//*[@label-field="{PORT_FORWARDING.SERVICE_NAME}"]/div[2]/div[1]/span[2]/input').send_keys("crypto")
 
-        ip = driver.find_element(By.XPATH,
-                                 '//*[@label-field="{PORT_FORWARDING.DEVICE_IP_ADDRESS}"]/div[2]/div[1]/span[2]/input')
-        ip.send_keys(private_ip)
+        driver.find_element(By.XPATH,
+                                 '//*[@label-field="{PORT_FORWARDING.DEVICE_IP_ADDRESS}"]/div[2]/div[1]/span[2]/input').send_keys(private_ip)
 
-        ex_port = driver.find_element(By.XPATH, '//*[@id="port-forwarding-external-port"]/div[2]/div[1]/span[2]/input')
-        in_port = driver.find_element(By.XPATH, '//*[@id="port-forwarding-internal-port"]/div[2]/div[1]/span[2]/input')
-
-        ex_port.send_keys(server_port)
-        in_port.send_keys(server_port)
+        driver.find_element(By.XPATH, '//*[@id="port-forwarding-external-port"]/div[2]/div[1]/span[2]/input').send_keys(server_port)
+        driver.find_element(By.XPATH, '//*[@id="port-forwarding-internal-port"]/div[2]/div[1]/span[2]/input').send_keys(server_port)
 
         driver.find_element(By.XPATH, '//*[@id="port-forwarding-grid-save-button"]/div[2]/div[1]/a').click()
 
